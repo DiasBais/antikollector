adefeccdb00525cc0babfa6c651d269214b711f5
Dias Baiseit <dias.baiseit.02@gmail.com>
2021-11-16 12:23:49 +0600

    'fixed'

---
+++ dist/lang/ru/lang.js
@@ -0,0 +1,20 @@
+export default function() {
+    return {
+        home_page: 'Главная страница',
+        our_advantages: 'Наши преимущества',
+        services: 'Услуги',
+        about_company: 'О компании',
+        reviews: 'Отзывы',
+        faq: 'FAQ',
+        we_will_protect_you_from_collectors_npi_and_mfos: 'Мы защитим Вас от Коллекторов, ЧСИ И МФО',
+        are_you_harassed_by_calls_and_threats: 'Вас беспокоят звонками и угрозами?',
+        are_you_haunted_by_the_collectors_at_work_and_at_home: 'Вас преследует у работы и дома коллекторы?',
+        are_you_being_morally_pressured: 'На Вас оказывают моральное давление?',
+        have_your_accounts_been_blocked: 'Ваши счета заблокировали?',
+        are_you_paid_salary_with_deductions: 'Вам выплачивают зп с удержаниями?',
+        have_your_credit_history_been_ruined: 'Вам испортили кредитную историю?',
+        we_have_a_solution: 'У НАС ЕСТЬ РЕШЕНИЕ!',
+        free: 'БЕСПЛАТНО',
+        protect_me: 'Защитить меня',
+    }
+}
