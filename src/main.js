ceb4803e34fe6ada799bba9d8d0e5cf5229986e2
Dias Baiseit <dias.baiseit.02@gmail.com>
2021-11-19 11:54:42 +0600

    'check'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+import VueSession from 'vue-session'
 
+Vue.use(VueSession)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
